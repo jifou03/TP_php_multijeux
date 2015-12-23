646e9e70f34db4e80e59a6c9381f7f4cae1f6a2b
Nickerson <nickerson.jbaptiste@hotmail.com>
2015-12-23 14:49:59 -0500

    modification ajout personne etc etc etc

--- add_inscription.php
+++ add_inscription.php
@@ -1,22 +1,30 @@
 <?php
 
-    $nomutilisateur = $_POST['nomutilisateur'];
+   /*$nomutilisateur = $_POST['nomutilisateur'];
     $password = $_POST['password'];
     $prenom = $_POST['prenom'];
     $nom = $_POST['nom'];
     $courriel = $_POST['courriel'];
     $sexe = $_POST['sexe'];
 
+    if ($sexe == 'garcon') {
+        $sexe = 'garcon';
+    } else if ($sexe == 'fille') {
+        $sexe = 'fille';
+    }
+
 
     $link = mysqli_connect("localhost", "root", "") or die("Couldn't connect wtf");
 
-    $db_selected = mysqli_select_db($link, 'multijeux') or die ("Couldn't connect AGAIN");
+    mysqli_select_db($link, 'multijeux') or die ("Couldn't connect AGAIN");
 
     $query = "INSERT INTO users (username, password, prenom, nom, courriel, sexe)
     VALUES ('$nomutilisateur', '$password', '$prenom', '$nom', '$courriel', '$sexe')";
 
-    mysqli_query($link, $query);
+    mysqli_query($link, $query);*/
 
+
+    //header('Location: http://localhost/tp_php_multijeux/index.php');
 
 ?>
 
