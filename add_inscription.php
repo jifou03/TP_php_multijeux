d8908c894363b5f2dce1eced9bd48f8229a10159
Nickerson <nickerson.jbaptiste@hotmail.com>
2015-12-23 12:51:15 -0500

    inscription ajouter et menu

--- add_inscription.php
+++ add_inscription.php
@@ -1,3 +1,21 @@
 <?php
 
+    $name = $_POST['nom'];
+
+    $link = mysqli_connect("localhost", "root", "") or die("Couldn't connect wtf");
+
+    $db_selected = mysqli_select_db($link, 'multijeux') or die ("Couldn't connect AGAIN");
+
+    $query = "INSERT INTO users  VALUES ('Nickerson')";
+
+    mysqli_query($link, $query);
+
+    $query = "SELECT * FROM users";
+
+    if ($result = mysqli_query($link, $query)) {
+        $row = mysqli_fetch_array($result);
+        print_r($row);
+    }
 ?>
+
+
--- inscription.php
+++ inscription.php
@@ -7,11 +7,29 @@ $site_data[PAGE_ID] = 'inscription'
 <head>
     <meta charset="UTF-8">
     <title>$Title$</title>
+    <link rel="stylesheet" type="text/css" href="css/site_style.css"/>
 </head>
 <body>
 <h1>$Title$</h1>
-<?php
-
-?>
+<form action="add_inscription.php" method="post">
+<ul>
+    <li><label class="inscription" for="nom">Nom :</label>
+        <input type="text" name="nom" id="nom"/></li>
+    <li><label class="inscription" for="prenom">Prenom :</label>
+        <input type="text" name="prenom" id="prenom"/></li>
+    <li><label class="inscription" for="nomutilisateur">Nom d'utilisateur :</label>
+        <input type="text" name="nomutilisateur" id="nomutilisateur"/></li>
+    <li><label class="inscription" for="password">Password :</label>
+        <input type="password" name="password" id="password"/></li>
+    <li><label class="inscription" for="courriel">Courriel :</label>
+        <input type="text" name="courriel" id="courriel"/></li>
+    <li><label for="sexe">Sexe :</label>
+        <label for="garcon">Garçon :</label>
+        <input type="radio" name="sexe" id="garcon"/>
+        <label for="fille">Fille :</label>
+        <input type="radio" name="sexe" id="fille"/></li>
+    <input type="submit" id="submit" name="submit" value="Soumettre"/>
+</ul>
+</form>
 </body>
 </html>
