bf2752bf00089a5372308c212327f1f19d31fb89
jifou03 <jf.pgrenier@gmail.com>
2015-12-23 14:56:37 -0500

    correction d'erreur

--- connection.php
+++ connection.php
@@ -5,8 +5,16 @@ require_once 'view_parts/_page_base.php'
 ?>
 
 <h1><?= ucfirst($site_data[PAGE_ID]); ?></h1>
-<?php
 
+<h2>Veuillez vous indentifier</h2>
+<form>
+    <label for="username">username</label>
+    <input type="text" id="username" name="username">
+    <label for="password">username</label>
+    <input type="text" id="password" name="password">
+
+
+</form>
+<?php
+require_once 'view_parts/_page_bottom.php'
 ?>
-</body>
-</html>
