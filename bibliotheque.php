dab71b74ce34abd083d8af9c900ccf1ba5272771
indatou <barryindatou@yahoo.fr>
2015-12-23 15:00:59 -0500

    push essai ajout onglet bibliotheque ajout plusieurs jeux dans dossier js

--- bibliotheque.php
+++ bibliotheque.php
@@ -1,26 +1,44 @@
 <?php
 require_once '_defines.php';
-require_once 'data/_main_data.php';
-$site_data[PAGE_ID] = 'index';
-require_once 'view_parts/_page_base.php';
-require_once 'view_parts/_header.php';
-require_once 'view_parts/_main_menu.php';
+$site_data[PAGE_ID] = 'bibliotheque'
 ?>
-<link rel="stylesheet" type="text/css" href="css/site_style.css" xmlns="http://www.w3.org/1999/html"/>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>$Title$</title>
+    <style>
+        p{
+            width:25%;
+            display: inline-block;
+            height: 200px;
+            /*border: 1px solid black;*/
+            margin: 40px;
 
-<div id="main">
-    <h1>Bienvenue sur Multijeux</h1>
-    <p>Si vous etes un nouveau visiteur de ce site de jeux, veuillez clique sur
-        le bouton ci-dessous afin de vous inscrire.
-    </p>
-
-    <a href="inscription.php"><input type="button" value="inscrivez-vous ici" ></a>
-
-    <P>Si vous est déja inscrit cliquez sur le bouton suivant pour vous connecter</P>
-
-    <a href="connection.php"><input type="button" value="connectez-vous ici" ></a>
-</div>
-
+        }
+        img{
+            width:377px;
+            height: 300px;
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+<h1>BIBLIOTHEQUE</h1>
 <?php
-require_once 'view_parts/_page_bottom.php';
+
 ?>
+<p><a href="js/doodle-jump-en-javascript/index.htm"><img src="js/doodle-jump-en-javascript/img/doodle_jeux.PNG" alt="image indisponible"/>
+        DOODLE JUMP</a></p>
+<p><a href="js/invasion-extraterrestre/invasion/invasion.html"><img src="js/invasion-extraterrestre/invasion/res/invasion.jpg" alt="image indisponible"/>
+        INVASION EXTRATERRESTRE</a></p>
+<p><a href="js/jeu-de-mot-facile/jsdico.html"><img src="js/jeu-de-mot-facile/jeux_de_mot.PNG" alt="image indisponible"/>
+        JEUX DE MOTS FACILE</a></p>
+<p><a href="js/jeux-du-serpent-snake/serpent/serpent%20html/serpent%20html.html"><img src="js/jeux-du-serpent-snake/serpent/serpent.PNG" alt="image indisponible"/>
+        JEUX DU SERPENT SNAKE</a></p>
+<p><a href="js/machine-a-sous/casino/machine.htm"><img src="js/machine-a-sous/casino/casino.PNG" alt="image indisponible"/>
+        MACHINE A SOUS</a></p>
+<p><a href="js/mastermind-iphone/iFUN/index.html"><img src="js/mastermind-iphone/mastermind.PNG" alt="image indisponible"/>
+        MASTERMIND</a></p>
+</body>
+</html>
