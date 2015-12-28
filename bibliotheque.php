a8c99235e2875adc1a2f63ecd432dd0574632b28
indatou <barryindatou@yahoo.fr>
2015-12-28 16:15:02 -0500

    push essai ajout onglet bibliotheque ajout plusieurs jeux dans dossier js ajout d'image dans bibliotheque.php ajout jeux ajout jeux dans dossier js ajout dossier bibliotheque et ajout page de redirection des differents jeux

--- bibliotheque.php
+++ bibliotheque.php
@@ -6,7 +6,7 @@ $site_data[PAGE_ID] = 'bibliotheque'
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>$Title$</title>
+    <title>Bibliotheque</title>
     <style>
         p{
             width:25%;
@@ -28,17 +28,31 @@ $site_data[PAGE_ID] = 'bibliotheque'
 <?php
 
 ?>
-<p><a href="js/doodle-jump-en-javascript/index.htm"><img src="js/doodle-jump-en-javascript/img/doodle_jeux.PNG" alt="image indisponible"/>
+<p><a href="bibliotheque/doodle_jump.php"><img src="js/doodle-jump-en-javascript/img/doodle_jeux.PNG" alt="image indisponible"/>
         DOODLE JUMP</a></p>
-<p><a href="js/invasion-extraterrestre/invasion/invasion.html"><img src="js/invasion-extraterrestre/invasion/res/invasion.jpg" alt="image indisponible"/>
+
+<p><a href="bibliotheque/invasion-extraterrestre.php"><img src="js/invasion-extraterrestre/invasion/res/invasion.jpg" alt="image indisponible"/>
         INVASION EXTRATERRESTRE</a></p>
-<p><a href="js/jeu-de-mot-facile/jsdico.html"><img src="js/jeu-de-mot-facile/jeux_de_mot.PNG" alt="image indisponible"/>
-        JEUX DE MOTS FACILE</a></p>
-<p><a href="js/jeux-du-serpent-snake/serpent/serpent%20html/serpent%20html.html"><img src="js/jeux-du-serpent-snake/serpent/serpent.PNG" alt="image indisponible"/>
-        JEUX DU SERPENT SNAKE</a></p>
-<p><a href="js/machine-a-sous/casino/machine.htm"><img src="js/machine-a-sous/casino/casino.PNG" alt="image indisponible"/>
+
+<p><a href="bibliotheque/jeu-de-mot-facile.php"><img src="js/jeu-de-mot-facile/jeux_de_mot.PNG" alt="image indisponible"/>
+        JEUX DE MOTS</a></p>
+
+<p><a href="bibliotheque/jeu-du-sepent-snake.php"><img src="js/jeux-du-serpent-snake/serpent/serpent.PNG" alt="image indisponible"/>
+        SNAKE</a></p>
+
+<p><a href="bibliotheque/jeu-memory.php"><img src="js/jeux-memory/jeuxCarte/jeux_memoire.png" alt="image indisponible" />
+        MEMOIRE</a></p>
+
+<p><a href="bibliotheque/machine-a-sous.php"><img src="js/machine-a-sous/casino/casino.PNG" alt="image indisponible"/>
         MACHINE A SOUS</a></p>
-<p><a href="js/mastermind-iphone/iFUN/index.html"><img src="js/mastermind-iphone/mastermind.PNG" alt="image indisponible"/>
+
+<p><a href="bibliotheque/mastermind.php"><img src="js/mastermind-iphone/mastermind.PNG" alt="image indisponible"/>
         MASTERMIND</a></p>
+
+<p><a href="bibliotheque/labyrinthe.php"><img src="js/labyrinthe/labyrinthe/labyrinthe.PNG" alt="image indisponible"/>
+        LABYRINTHE</a></p>
+
+<p><a href="bibliotheque/moby-bros.php"><img src="js/moby-bros-l-aventurier-des-etoiles/moby.PNG" alt="image indisponible"/>
+        MOBY BROS</a></p>
 </body>
 </html>
---
+++ js/moby-bros-l-aventurier-des-etoiles/readme.txt
@@ -0,0 +1,46 @@
+Moby bros : l'aventurier des étoiles------------------------------------
+Url     : http://codes-sources.commentcamarche.net/source/21826-moby-bros-l-aventurier-des-etoilesAuteur  : cs_LiBe444Date    : 24/08/2013
+Licence :
+=========
+
+Ce document intitulé « Moby bros : l'aventurier des étoiles » issu de CommentCaMarche
+(codes-sources.commentcamarche.net) est mis à disposition sous les termes de
+la licence Creative Commons. Vous pouvez copier, modifier des copies de cette
+source, dans les conditions fixées par la licence, tant que cette note
+apparaît clairement.
+
+Description :
+=============
+
+Je vous pr&eacute;sente un jeu qui aurait pu &ecirc;tre une parodie du script ma
+rio bros, pourtant, je l'ai r&eacute;alis&eacute; tout seul.
+<br />
+<br />Il c
+onsiste en une page html contenant une iframe : dessus, on peut faire balader 'l
+ittle idiot character' sur des plateformes...
+<br />Attention &agrave; ne pas l
+e faire tomber, et attention &agrave; ce qu'il ne se fasse pas bouffer par un cr
+ocodile, sinon vous recommencez.
+<br />
+<br />Sans PHP, il n'y a qu'un seul mo
+yen de jouer &agrave; tous les niveaux : vous devez modifier &lt;script src=levX
+.js&gt; en &lt;script src=levX+1.js&gt;.
+<br />
+<br />Amusez vous bien
+<br />
+<a name='conclusion'></a><h2> Conclusion : </h2>
+<br />Ce script est une d&eac
+ute;monstration que l'on peut r&eacute;sumer des programmes compliqu&eacute;s en
+ quelque chose de tr&egrave;s simple ! (le script prend dans les cinq six lignes
+).
+<br />
+<br />RECOMMANDATION : NE SOYEZ PAS SEVERE, JE DETESTE CA !!
+<br />
+Ou alors ne me notez pas abusivement... &ccedil;a me d&eacute;courage de poster 
+des sources.
+<br />
+<br />Mon casse-briques &eacute;tait du travail bacl&eacut
+e;, je sais... quand m&ecirc;me, UN sur DIX, &ccedil;a veut dire que je ne m&eac
+ute;rite pas d'&ecirc;tre sur ce site...
+<br />Est-ce que vous le pensez vraime
+nt? Existe-t-il des gens ingrats pour nier le travail de gens comme nous ?
