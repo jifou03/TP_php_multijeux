9bda54ba3dc2cfb84fe891141d8178d6e853710d
jifou03 <jf.pgrenier@gmail.com>
2016-01-12 00:28:22 -0500

    correction menu

--- test_connect.php
+++ test_connect.php
@@ -1,5 +1,4 @@
 <?php
-echo 'allo';
 $link = mysqli_connect("localhost", "multijeux", "multijeux") or die("Couldn't connect wtf");
 mysqli_select_db($link, 'p62a15tpinda_multijeuxdb') or die ("Couldn't connect AGAIN");
 
@@ -12,7 +11,7 @@ $get_user_info = mysqli_fetch_array($result);
 //var_dump($get_user_info);
 
 if (array_key_exists('password', $_POST) AND array_key_exists('username', $_POST)) {
-    if ( !empty($_POST['password'] AND !empty($_POST['username'])) ){
+    if ( ! empty($_POST['password'] AND ! empty($_POST['username'])) ){
         if ($_POST['username'] == $get_user_info['username'] AND sha1($_POST['password']) == $get_user_info['password']) {
             session_name("h8278");
             session_start();
