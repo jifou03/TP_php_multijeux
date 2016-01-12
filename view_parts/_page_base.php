387647f35ba38f6e92dce9c6e5946c4959d463c5
jifou03 <jf.pgrenier@gmail.com>
2016-01-11 20:08:47 -0500

    insertion du favicon

--- view_parts/_page_base.php
+++ view_parts/_page_base.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title><?php echo SITE_NAME . ' - ' . ucfirst($site_data[PAGE_ID]) ?></title>
     <link rel="stylesheet" type="text/css" href="css/site_style.css"/>
+    <link rel="icon" href="images/index/multijeux.ico" type="image/x-icon">
 </head>
 <body>
 <div id="wrapper">
