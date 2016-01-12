a2d73928ed342a609034ce62cab896d802dca755
jifou03 <jf.pgrenier@gmail.com>
2016-01-11 21:48:57 -0500

    connection a la bd sentora- prise 9

--- test_connect.php
+++ test_connect.php
@@ -1,13 +1,13 @@
 <?php
     $link = mysqli_connect("localhost", "multijeux", "multijeux") or die("Couldn't connect wtf");
-    mysqli_select_db($link, 'users') or die ("Couldn't connect AGAIN");
+    mysqli_select_db($link, 'p62a15tpinda_multijeuxdb') or die ("Couldn't connect AGAIN");
 
     $username = $_POST['username'];
 
     $query = "SELECT * FROM users WHERE username = '$username'";
     $result = mysqli_query($link, $query);
     $get_user_info = mysqli_fetch_array($result);
-    var_dump($get_user_info);
+    //var_dump($get_user_info);
 
     if (array_key_exists('password', $_POST) AND array_key_exists('username', $_POST)) {
         if ( ! empty($_POST['password'] AND ! empty($_POST['username'])) ){
