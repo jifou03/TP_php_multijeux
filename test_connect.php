c5b2daa44d5e1672273ebfedcf4c97f56b3295b8
jifou03 <jf.pgrenier@gmail.com>
2016-01-12 00:18:28 -0500

    correction menu

--- test_connect.php
+++ test_connect.php
@@ -1,4 +1,5 @@
 <?php
+echo 'allo';
 $link = mysqli_connect("localhost", "multijeux", "multijeux") or die("Couldn't connect wtf");
 mysqli_select_db($link, 'p62a15tpinda_multijeuxdb') or die ("Couldn't connect AGAIN");
 
